57e29c4bb164fdc070359de58a717052b8b5e30b
Raffaele <raffaelecpl@gmail.com>
2024-10-02 15:59:48 +0200

    Creata schermata momentanea

--- __main__.py
+++ __main__.py
@@ -1,1 +1,25 @@
 import pygame
+import sys
+
+#SETTAGGIO FINESTRA
+
+pygame.init()
+
+orologio = pygame.time.Clock()
+risoluzione = (800,600)
+schermo = pygame.display.set_mode(risoluzione)
+pygame.display.set_caption('Stronghold Domination')
+orologio.tick(60)  
+
+
+running = True
+
+while running:
+    schermo.fill("white")
+    pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+            sys.exit()
